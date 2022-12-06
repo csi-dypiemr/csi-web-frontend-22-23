d01946bb6f789f2bdde934641815a203d1b98275
Sagar Gajare <56780589+Sagargajare@users.noreply.github.com>
2022-12-06 12:35:00 +0530

    created components

---
+++ components/Team.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+function Team({}: Props) {
+  return <div>Team</div>;
+}
+
+export default Team;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,19 @@
 import { Container, Heading } from '@chakra-ui/react';
 import { ReactElement } from 'react';
+import Event from '../components/Event';
+import Contact from '../components/Contact';
+import Hero from '../components/Hero';
 import DefaultLayout from '../components/layouts/DefaultLayout';
+import Team from '../components/Team';
 import type { NextPageWithLayout } from './_app';
 
 const Home: NextPageWithLayout = () => {
   return (
     <Container>
-      <Heading>Hello CSI</Heading>
+      <Hero />
+      <Team />
+      <Event />
+      <Contact />
     </Container>
   );
 };
