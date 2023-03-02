94f6d27ac9b4634b458a1c6e674dbd4b5acb0c7b
vikrant-khedkar <vikrantkhedkar2720@gmail.com>
2023-03-02 11:51:38 +0530

    update

--- components/TeamSec.tsx
+++ components/TeamSec.tsx
@@ -1,10 +1,9 @@
-import { Container } from '@chakra-ui/react';
+import { Container, Text } from '@chakra-ui/react';
 import React from 'react';
 // import { map } from 'rxjs/operators';
-import data from '../public/data.js'
+import data from '../public/data.js';
 
 import TeamCard from './TeamCard';
-
 
 // const breakpoints = {
 //   sm: '480px',
@@ -14,15 +13,36 @@ import TeamCard from './TeamCard';
 //   xxl: '1440px',
 // }
 
-
-
 type Props = {};
-// var map = new Map();  
-const TeamCards = data.map(card =>{
-    return <TeamCard name={card.name} pic={card.pic} position={card.position}/>
-})
+// var map = new Map();
+const TeamCards = data.map((card) => {
+  return <TeamCard name={card.name} pic={card.pic} position={card.position} />;
+});
 const TeamSec = (props: Props) => {
-  return <div style={{display:'flex' ,backgroundColor:'#181823'}} >{TeamCards}</div>;
+  return (
+    <div style={{ backgroundColor: '#181823' }}>
+      <Text
+        fontSize="3xl"
+        ml="40px"
+        as="b"
+        style={{ color: 'white', display: 'flex' }}
+      >
+        MEET OUR TEAM
+      </Text>
+      <hr
+        style={{
+          width: '300px',
+          backgroundColor: 'white',
+          marginLeft: '40px',
+          height: '3px',
+        }}
+      ></hr>
+      <div style={{ display: 'flex', backgroundColor: '#181823' }}>
+        {TeamCards}
+      </div>
+      
+    </div>
+  );
 };
 
 export default TeamSec;
